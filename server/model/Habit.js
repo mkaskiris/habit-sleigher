06e14fd77d155c3540b02643750f82242180f1ce
Jakirul <jakirul.islam@live.co.uk>
2021-12-13 21:23:56 +0000

    Started adding streaks

--- server/model/Habit.js
+++ server/model/Habit.js
@@ -36,18 +36,60 @@ module.exports = class Habit {
         })
     }
 
+    // static getOldHabits(id) {
+    //     return new Promise (async (resolve, reject) => { 
+    //         try {
+    //             const arr = []
+               
+    //             const dayBefore = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = $1 AND time_done::DATE = current_date - 1;`, [id]);
+    //             const dayBefore2 = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = $1 AND time_done::DATE = current_date - 2;`, [id]);
+    //             const dayBefore3 = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = $1 AND time_done::DATE = current_date - 3;`, [id]);
+                
+    //             arr.push(parseInt(dayBefore.rows[0].count))
+    //             arr.push(parseInt(dayBefore2.rows[0].count))
+    //             arr.push(parseInt(dayBefore3.rows[0].count))
+    //             resolve(arr)
+
+    //         } catch(err) {
+    //             reject("Error getting old habits: ", err)
+    //         }
+    //     })
+    // }
+
     static getOldHabits(id) {
         return new Promise (async (resolve, reject) => { 
             try {
                 const arr = []
                
+                const days = await db.query("SELECT date_trunc('day', now() - currTime::date) FROM habit WHERE habit_id = $1", [id]);
+
+                let difference = days.rows[0].date_trunc.days;
+
                 const dayBefore = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = $1 AND time_done::DATE = current_date - 1;`, [id]);
                 const dayBefore2 = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = $1 AND time_done::DATE = current_date - 2;`, [id]);
                 const dayBefore3 = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = $1 AND time_done::DATE = current_date - 3;`, [id]);
                 
-                arr.push(parseInt(dayBefore.rows[0].count))
-                arr.push(parseInt(dayBefore2.rows[0].count))
-                arr.push(parseInt(dayBefore3.rows[0].count))
+                if (difference == undefined) {
+                    
+                    resolve(arr);
+                } else if (difference == 1) {
+                    arr.push(parseInt(dayBefore.rows[0].count))
+                  
+                    resolve(arr)
+                } else if (difference == 2) {
+                    arr.push(parseInt(dayBefore.rows[0].count))
+                    arr.push(parseInt(dayBefore2.rows[0].count))
+                 
+                    resolve(arr)
+                } else if (difference == 3) {
+                    arr.push(parseInt(dayBefore.rows[0].count))
+                    arr.push(parseInt(dayBefore2.rows[0].count))
+                    arr.push(parseInt(dayBefore3.rows[0].count))
+                   
+                    resolve(arr)
+                }
+                
+
                 resolve(arr)
 
             } catch(err) {
@@ -60,8 +102,8 @@ module.exports = class Habit {
         return new Promise (async (resolve, reject) => {
             try {
                 const getUser = await db.query("SELECT user_id FROM user_table WHERE username = $1", [username])
-              
-                const newHabit = await db.query("INSERT INTO habit (habit, currFreq, frequency, currTime, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [habit, 0, frequency, new Date().toLocaleDateString('en-GB'), getUser.rows[0].user_id])
+                
+                const newHabit = await db.query("INSERT INTO habit (habit, currFreq, frequency, user_id) VALUES ($1, $2, $3, $4) RETURNING *;", [habit, 0, frequency, getUser.rows[0].user_id])
                 let habit1 = new Habit(newHabit.rows[0]);
                 resolve(habit1)
         
@@ -72,18 +114,7 @@ module.exports = class Habit {
 
     }
 
-    static deleteHabit(id) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await db.query("DELETE FROM habit WHERE habit_id = $1;", [id])
-                resolve('habit deleted')
-            } catch (err) {
-                reject("Error deleting habit: ", err)
-            }
-        })
-    }
-
-    static getHabits(habit_id, username) {
+    static appendFreq(habit_id) {
         return new Promise(async (resolve, reject) => {
             try {
               
@@ -102,6 +133,17 @@ module.exports = class Habit {
         })
     }
 
+    static deleteHabit(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await db.query("DELETE FROM habit WHERE habit_id = $1;", [id])
+                resolve('habit deleted')
+            } catch (err) {
+                reject("Error deleting habit: ", err)
+            }
+        })
+    }
+
     static getHabits(habit_id, user_id) {
         return new Promise(async (resolve, reject) => {
             try {
@@ -115,7 +157,6 @@ module.exports = class Habit {
                 const maxFreq = await db.query("SELECT frequency FROM habit WHERE habit_id = $1", [habit_id])
                 const newArray= allLists.rows.map(element => element.count);
                 
-<<<<<<< HEAD
                 let count = 0;
                 for (let i = 0; i < newArray.length; i++) {
                     if (parseInt(newArray[i]) === parseInt(maxFreq.rows[0].frequency)) {
@@ -128,18 +169,12 @@ module.exports = class Habit {
                 }
 
                 if (currStreak.rows[0].currstreak >= maxStreak.rows[0].maxstreak) {
-                    let changed = await db.query("UPDATE habit SET maxstreak = $1 WHERE habit_id = $2", [currStreak.rows[0].currstreak, habit_id]);
-                    console.log(changed, habit_id)
+                    await db.query("UPDATE habit SET maxstreak = $1 WHERE habit_id = $2", [currStreak.rows[0].currstreak, habit_id]);
                 }
 
                 const getUser = await db.query("SELECT user_id FROM user_table WHERE username = $1", [user_id])
     
                 const data = await db.query("SELECT * FROM habit WHERE user_id = $1 ORDER BY habit_id DESC", [parseInt(getUser.rows[0].user_id)])
-=======
-                const getUser = await db.query("SELECT user_id FROM user_table WHERE username = $1;", [username])
-
-                const data = await db.query("SELECT * FROM habit WHERE user_id = $1 ORDER BY habit_id DESC;", [parseInt(getUser.rows[0].user_id)])
->>>>>>> 517cde308ba9935356862757edbeb22b59671578
                 
                 resolve(data)
                 
@@ -150,28 +185,22 @@ module.exports = class Habit {
         })
     }
 
-    static createHabitEntry(data) {
+    static newHabitEntry(data) {
         return new Promise(async (resolve, reject) => {
           try {
             const habitMaxCounter = await db.query(`SELECT COUNT(*) FROM habit_counter WHERE habit_id = ${data.habit_id} AND time_done::DATE = current_date `)
             const habitFrequency = await db.query(`SELECT frequency FROM habit WHERE habit_id = ${data.habit_id}`)
             if (habitMaxCounter.rows[0].count < habitFrequency.rows[0].frequency) {
-<<<<<<< HEAD
                 const updateHabit = await db.query("UPDATE habit SET currfreq = $1 WHERE habit_id = $2 RETURNING *;", [parseInt(habitMaxCounter.rows[0].count) + 1, data.habit_id])
                 const insertHabitCounter = await db.query(`INSERT INTO habit_counter (habit_id, time_done) VALUES (${data.habit_id}, '${data.date}') RETURNING *;`);
 
-=======
-                await db.query("UPDATE habit SET currfreq = $1 WHERE habit_id = $2;", [parseInt(habitMaxCounter.rows[0].count) + 1, data.habit_id])
-                const insertHabitCounter = await db.query(`INSERT INTO habit_counter (habit_id, time_done, finished) VALUES (${data.habit_id}, '${data.date}', ${data.finished}) RETURNING *;`);
-              
->>>>>>> 517cde308ba9935356862757edbeb22b59671578
                 const newHabitEntry = insertHabitCounter.rows[0];
                 resolve(newHabitEntry)
             } else  {
-              throw new Error('Too many habits')
+              reject('ERROR!')
             } 
           } catch (error) {
-                reject(`Could not create a new habit entry! Try again: ${error}`);
+            reject(`Could not create a new habit entry! Try again`);
           }
         });
       }
