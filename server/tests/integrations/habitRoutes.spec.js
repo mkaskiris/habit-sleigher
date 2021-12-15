6df28f950f05941d5a8d3e885b60737b4d4028f2
Jakirul <jakirul.islam@live.co.uk>
2021-12-15 15:35:06 +0000

    2

--- server/tests/integrations/habitRoutes.spec.js
+++ server/tests/integrations/habitRoutes.spec.js
@@ -26,8 +26,7 @@ describe('habit endpoints', ()=>{
         jest.spyOn(auth, 'verifyToken').mockImplementation((req, res, next) => next());
         let authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjM5NTgwOTgzfQ.VJRbDqHvnQw3CMVg36n8JNbkHJlIw6heVYSNlkldylI"
         const res = await request(api).get('/habits').set('Authorization','Bearer '+ authToken)
-        expect(res).toEqual(12)
         expect(res.statusCode).toEqual(200)
-        expect(res.body.length).toEqual(3)
+        expect(res.body.length).toEqual(6)
     })
 })
